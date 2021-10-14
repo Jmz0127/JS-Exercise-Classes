d9480739009b55e1f99c96a3e85dffa04673f58b
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2021-10-14 15:19:40 -0400

    finished task 3

--- index.js
+++ index.js
@@ -108,7 +108,16 @@ class Car {
         + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
         + {name} and {location} of course come from the instance's own properties.
 */
-class Lambdasian {}
+class Lambdasian {
+	constructor(attribute) {
+		this.name = attribute.name;
+		this.age = attribute.age;
+		this.location = attribute.location;
+	}
+	speak() {
+		return `Hello my name is ${this.name}, I am from ${this.location}`;
+	}
+}
 
 /*
   TASK 4
